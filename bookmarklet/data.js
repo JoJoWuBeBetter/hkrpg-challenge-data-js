c9bb7255da1a97ab37501aea316ff48a4363a30c
JoJoWu <453840856@qq.com>
2024-07-23 12:00:41 +0800

    修复获取角色数据错误的BUG

--- bookmarklet/data.js
+++ bookmarklet/data.js
@@ -1,7 +1,61 @@
-// 定义一个函数，用于打印带参数的问候语  
-function helloWorld(name) {  
-    alert("Hello, " + name + "!"); 
-}  
+var roleData;
+loadScript(
+  "https://cdn.bootcdn.net/ajax/libs/blueimp-md5/2.18.0/js/md5.min.js",
+  getData
+);
 
-// 调用函数并传递参数  
-helloWorld("JoJoWu");  
+function getData() {
+  roleData = getUserGameRolesByCookie();
+}
+
+function getUserGameRolesByCookie() {
+  var url =
+    "https://api-takumi.mihoyo.com/binding/api/getUserGameRolesByCookie?game_biz=hkrpg_cn";
+  return new Promise((resolve, reject) => {
+    fetch(url, {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        Cookie: getCookie(),
+      },
+    })
+      .then((res) => {
+        if (res.retcode != 0) {
+          throw new Error("请求错误：" + res.message);
+        }
+        return res['data']['list'][0];
+      })
+      .catch((error) => {
+        console.error("用户信息请求失败:", error);
+        reject(error); // 如果出错，拒绝 Promise
+      });
+  });
+}
+function getCookie() {
+  var cookie = document.cookie;
+  return cookie;
+}
+
+function loadScript(url, callback) {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+
+  // 检查浏览器支持的 script 加载方式
+  if (script.readyState) {
+    // For IE
+    script.onreadystatechange = function () {
+      if (script.readyState === "loaded" || script.readyState === "complete") {
+        script.onreadystatechange = null;
+        callback();
+      }
+    };
+  } else {
+    // Other browsers
+    script.onload = function () {
+      callback();
+    };
+  }
+
+  script.src = url;
+  document.head.appendChild(script);
+}
